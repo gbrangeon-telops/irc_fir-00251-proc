ee12424cc00dcc47d63d87e8c2474103876a16c7
enofodjie-telpos <edem.nofodjie@telops.com>
2016-02-22 17:43:46 +0000

    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@18080 3e98269d-ff51-e449-abca-491a6567d830

--- src/FPA/isc0207A/HDL/FPA_define.vhd
+++ src/FPA/isc0207A/HDL/FPA_define.vhd
@@ -39,6 +39,7 @@ package FPA_define is
    constant DEFINE_FPA_BITSTREAM_LENGTH           : natural   := 58;       -- nombre de bits contenu  dans le bitstream de configuration serielle
    constant DEFINE_FPA_INT_TIME_OFFSET_nS         : natural   := 800;      -- int_time offset de 0.8usec 
    constant DEFINE_FPA_PROG_INT_TIME              : natural   := 100;      -- en coups d'horloge FPA, c'est le temps d'integration utilisé piour les images post configuration du detecteur 
+   constant DEFINE_FPA_XTRA_TRIG_INT_TIME         : natural   := 100;      -- en coups d'horloge FPA, c'est le temps d'integration utilisé pour les images xtra trig
    
    -- quelques caractéristiques du FPA
    constant DEFINE_FPA_INT_TIME_MIN_US            : integer   := 1; 
