59b95b5307882d808f47a1dea98567f5f17583ef
dalain-telpos <denis.alain@telops.com>
2016-02-22 14:32:53 +0000

    Release 1.4.2.0
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@18066 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_hawkA/svnrevs.pl
+++ bin/fir_00251_proc_hawkA/svnrevs.pl
@@ -1,5 +1,5 @@
-our $rel_proc_hw_rev = 17968;
-our $rel_proc_sw_rev = 17968;
-our $rel_proc_boot_rev = 17902;
-our $rel_proc_common_rev = 17904;
+our $rel_proc_hw_rev = 18065;
+our $rel_proc_sw_rev = 18065;
+our $rel_proc_boot_rev = 18065;
+our $rel_proc_common_rev = 18059;
 1;
