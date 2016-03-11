ddc346b8094fc223d8cfe1c10778e95c8f12471a
ssavary-telpos <simon.savary@telops.com>
2016-03-11 16:30:30 +0000

    Release 1.4.x.4
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@18215 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_herculesD/svnrevs.pl
+++ bin/fir_00251_proc_herculesD/svnrevs.pl
@@ -1,5 +1,5 @@
-our $rel_proc_hw_rev = 17885;
-our $rel_proc_sw_rev = 17885;
-our $rel_proc_boot_rev = 17818;
-our $rel_proc_common_rev = 17826;
+our $rel_proc_hw_rev = 18214;
+our $rel_proc_sw_rev = 18214;
+our $rel_proc_boot_rev = 18214;
+our $rel_proc_common_rev = 18208;
 1;
--- bin/fir_00251_proc_marsD/svnrevs.pl
+++ bin/fir_00251_proc_marsD/svnrevs.pl
@@ -1,5 +1,5 @@
 our $rel_proc_hw_rev = 18121;
-our $rel_proc_sw_rev = 18136;
-our $rel_proc_boot_rev = 18136;
-our $rel_proc_common_rev = 18129;
+our $rel_proc_sw_rev = 18214;
+our $rel_proc_boot_rev = 18214;
+our $rel_proc_common_rev = 18208;
 1;
