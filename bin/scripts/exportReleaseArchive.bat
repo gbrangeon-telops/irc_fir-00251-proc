f5edf20a6e716fbf87b47e70120188acfe223e19
dalain-telpos <denis.alain@telops.com>
2016-05-20 19:56:51 +0000

    Release batch file has been modified.
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@18755 3e98269d-ff51-e449-abca-491a6567d830

--- bin/scripts/exportReleaseArchive.bat
+++ bin/scripts/exportReleaseArchive.bat
@@ -1,10 +1,13 @@
 set releaseDir="%binDir%\Release_%firmwareVersion:.=_% (%sensorName%)"
 set paperworkTemplateDir=%scriptsDir%\paperwork\template
+set ntxminiFile=CommonTEL2000LibProject_xml_%xmlVersion%_%sensorWidth%x%sensorHeight%.exe
+set fubatch=%releaseDir%\Release_%firmwareVersion:.=_%.bat
 
 rem Clean up
 del %paperworkTemplateDir%.zip
 rd %releaseDir% /s /q
 
+rem Prepare firmware package
 mkdir %releaseDir%\FIR-00251-Proc\
 copy %binDir%\prom\*.* %releaseDir%\FIR-00251-Proc\
 
@@ -15,7 +18,7 @@ mkdir %releaseDir%\FIR-00257-Storage\
 copy %storageDir%\bin\prom\*.* %releaseDir%\FIR-00257-Storage\
 
 mkdir %releaseDir%\FIR-00251-NTx-Mini\
-copy %ntxminiDir%\CommonTEL2000LibProject_xml_%xmlVersion%_%sensorWidth%x%sensorHeight%.exe %releaseDir%\FIR-00251-NTx-Mini\CommonTEL2000LibProject_xml_%xmlVersion%_%sensorWidth%x%sensorHeight%.exe
+copy %ntxminiDir%\%ntxminiFile% %releaseDir%\FIR-00251-NTx-Mini\%ntxminiFile% 
 
 %x_xilperl% %scriptsDir%\paperwork\generatePaperwork.pl -sensor %sensorName% ^
    -v %firmwareVersion% -o %outputRevFile% -storage_revs %storageRevFile% -p %revFile% -x %xmlVersion% -fs %flashSettingsVersion% -fdv %flashDynamicValuesVersion% -t %paperworkTemplateDir%
@@ -24,4 +27,31 @@ copy %ntxminiDir%\CommonTEL2000LibProject_xml_%xmlVersion%_%sensorWidth%x%sensor
 
 move %paperworkTemplateDir%.zip %releaseDir%\Release_%firmwareVersion:.=_%.xlsx
 
-echo F:\Production\IRCAM\Firmwares\tsirfu_batch.bat %firmwareVersion%> %releaseDir%\Release_%firmwareVersion:.=_%.bat
+rem Generate firmware updater batch file
+echo @echo off> %fubatch%
+echo.>> %fubatch%
+echo set fu_exe=tsirfu.exe>> %fubatch%
+echo set fu_retry=0 >> %fubatch%
+echo.>> %fubatch%
+echo :findfu>> %fubatch%
+echo if not exist %%fu_exe%% (>> %fubatch%
+echo    if %%fu_retry%% == 2 (>> %fubatch%
+echo       echo Error: Cannot find firmware updater.>> %fubatch%
+echo       pause>> %fubatch%
+echo       goto end>> %fubatch%
+echo    )>> %fubatch%
+echo    set fu_exe=..\%%fu_exe%%>> %fubatch%
+echo    set /a fu_retry+=1 >> %fubatch%
+echo    goto findfu>> %fubatch%
+echo )>> %fubatch%
+echo.>> %fubatch%
+echo FIR-00251-NTx-Mini\%ntxminiFile%>> %fubatch%
+echo if not %%errorlevel%% == 0 goto end>> %fubatch%
+echo.>> %fubatch%
+echo tsirfu.exe -p p FIR-00251-Proc\fir_00251_proc_%sensorName%.mcs>> %fubatch%
+echo tsirfu.exe -p o FIR-00251-Output\fir_00251_output.mcs>> %fubatch%
+echo tsirfu.exe -p s FIR-00257-Storage\fir_00257_storage.mcs>> %fubatch%
+echo.>> %fubatch%
+echo pause>> %fubatch%
+echo.>> %fubatch%
+echo :end>> %fubatch%
