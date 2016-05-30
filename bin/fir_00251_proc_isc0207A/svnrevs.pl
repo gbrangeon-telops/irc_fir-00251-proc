4a2563b92b1de651a7b18d636f21d878f1c7d113
ssavary-telpos <simon.savary@telops.com>
2016-05-30 15:11:29 +0000

    Release 1.6.1.1
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@18778 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_isc0207A/svnrevs.pl
+++ bin/fir_00251_proc_isc0207A/svnrevs.pl
@@ -1,5 +1,5 @@
 our $rel_proc_hw_rev = 18706;
-our $rel_proc_sw_rev = 18728;
-our $rel_proc_boot_rev = 18728;
-our $rel_proc_common_rev = 18724;
+our $rel_proc_sw_rev = 18777;
+our $rel_proc_boot_rev = 18777;
+our $rel_proc_common_rev = 18771;
 1;
