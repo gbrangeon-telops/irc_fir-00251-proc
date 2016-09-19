9d3a0b4e3e24fa545cbdb51fda271eddb9618385
dalain-telpos <denis.alain@telops.com>
2016-09-19 13:27:59 +0000

    Pre-release 2.0.x.1
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@19200 3e98269d-ff51-e449-abca-491a6567d830

--- src/BuildInfo/pelicanD/BuildInfo.h
+++ src/BuildInfo/pelicanD/BuildInfo.h
@@ -4,7 +4,7 @@
 #define SVN_HARDWARE_REV      18728
 #define SVN_SOFTWARE_REV      18731
 #define SVN_BOOTLOADER_REV    18728
-#define SVN_COMMON_REV        18974
+#define SVN_COMMON_REV        19152
 
 #define SVN_UNCOMMITTED_CHANGES  ((SVN_HARDWARE_REV < 0) || (SVN_SOFTWARE_REV < 0) || (SVN_BOOTLOADER_REV < 0) || (SVN_COMMON_REV < 0))
 
@@ -12,7 +12,7 @@
 #warning Uncommitted changes detected.
 #endif
 
-#define HARDWARE_MISMATCH (1)
+#define HARDWARE_MISMATCH (0)
 
 #if HARDWARE_MISMATCH
 #error D:\Telops\FIR-00251-Proc\sdk\fir_00251_proc_pelicanD\hw_platform_0\fir_00251_proc_pelicanD.bit does not match D:\Telops\FIR-00251-Proc\sdk\fir_00251_proc_pelicanD\hw\fir_00251_proc_pelicanD.bit.
