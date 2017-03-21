e9a525df01dcb58c3567fd860980b1e62303d13f
odionne-telpos <olivier.dionne@telops.com>
2017-03-21 19:00:35 +0000

    mise à jour testbench du flagging
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@20202 3e98269d-ff51-e449-abca-491a6567d830

--- src/Flagging/Sim/src/runsim.do
+++ src/Flagging/Sim/src/runsim.do
@@ -1,4 +1,4 @@
 acom -O3 -work flagging_tb -2002  $dsn/../HDL/flag_define.vhd $dsn/../HDL/flagging_mblaze_intf.vhd $dsn/../HDL/flagging_SM.vhd $dsn/../HDL/flagging_top.bde $dsn/../../../../FIR-00251-Common/VHDL/tel2000pkg.vhd $dsn/../../../../Common_HDL/Utilities/double_sync.vhd $dsn/../../../../Common_HDL/Utilities/double_sync_vector.vhd $dsn/../../../../Common_HDL/Utilities/sync_reset.vhd $dsn/src/flagging_tb.bde $dsn/src/ublaze_sim.vhd
 asim -O5 +access +r +m+flagging_tb flagging_tb flagging_tb
 do waveform.do
-run 2 us
+run 5 s
--- src/Flagging/Sim/src/ublaze_sim.vhd
+++ src/Flagging/Sim/src/ublaze_sim.vhd
@@ -65,126 +65,113 @@ begin
 rstn_i <= '1' after 500 ns;
 Rstn <= rstn_i;
 
-   CLK100_GEN : process
-   begin          
-      clk100_i <= '1';
+CLK100_GEN : process
+begin          
+   clk100_i <= '1';
    wait for 5 ns;
-      clk100_i <= '0'; 
+   clk100_i <= '0'; 
    wait for 5 ns;
-   end process;    
-   CLK100 <= clk100_i;
-
+end process;    
+   
+CLK100 <= clk100_i;
 Feedback_FPA_Info <= feedback_fpa_info_i;
 
 FEEDBACK_FPA : process (clk100_i)
-    begin
-        if rising_edge(clk100_i) then
-			if (rstn_i = '0') then
-				feedback_fpa_info_i.exp_feedbk <= '0';
-				feedback_fpa_info_i.exp_info.exp_dval <= '0';
-				feedback_fpa_info_i.frame_id <= (others => '0');
-				feedback_fpa_info_i.exp_info.exp_time <= (others => '0');
-				feedback_fpa_info_i.exp_info.exp_indx	<= (others => '0');
+begin
+   if rising_edge(clk100_i) then
+	   if (rstn_i = '0') then
+   		feedback_fpa_info_i.exp_feedbk <= '0';
+   		feedback_fpa_info_i.exp_info.exp_dval <= '0';
+   		feedback_fpa_info_i.frame_id <= (others => '0');
+   		feedback_fpa_info_i.exp_info.exp_time <= (others => '0');
+   		feedback_fpa_info_i.exp_info.exp_indx	<= (others => '0');
+         busy_vector <= (others => '0');
+   	else
+   		if busy_vector(2) = '1' then
+   			feedback_fpa_info_i.exp_feedbk <= '1';
+   			feedback_fpa_info_i.exp_info.exp_dval <= '1';
+   			feedback_fpa_info_i.frame_id <= feedback_fpa_info_i.frame_id + 1;
+   			feedback_fpa_info_i.exp_info.exp_time <= exp_time;
+   			feedback_fpa_info_i.exp_info.exp_indx <= (others => '0');
             busy_vector <= (others => '0');
-			else
-				if busy_vector(2) = '1' then
-					feedback_fpa_info_i.exp_feedbk <= '1';
-					feedback_fpa_info_i.exp_info.exp_dval <= '1';
-					feedback_fpa_info_i.frame_id <= feedback_fpa_info_i.frame_id + 1;
-					feedback_fpa_info_i.exp_info.exp_time <= exp_time;
-					feedback_fpa_info_i.exp_info.exp_indx <= (others => '0');
-               busy_vector <= (others => '0');
-            else
-               busy_vector <= busy_vector + 1;
-					feedback_fpa_info_i.exp_feedbk <= '0';
-					feedback_fpa_info_i.exp_info.exp_dval <= '0';
-            end if;	
-			end if;
-				
-		end if;
+         else
+            busy_vector <= busy_vector + 1;
+   			feedback_fpa_info_i.exp_feedbk <= '0';
+   			feedback_fpa_info_i.exp_info.exp_dval <= '0';
+         end if;	
+   	end if;
+   end if;
 end process FEEDBACK_FPA;
-
-TRIG_HW_PROC: process is
-   begin
-      Trig_HW <= '0';
-      wait for 10 ns;
-      Trig_HW <= '1';
-      wait for 20 ns;
-      Trig_HW <= '0';
-      wait for 10 ns;
-      Trig_HW <= '1';
-      wait for 30 ns;
-      Trig_HW <= '0';
-      wait for 10 ns;
-end process TRIG_HW_PROC;
    
 ublaze_stim: process is
-   begin
-      AXIL_MOSI.awaddr <= (others => '0');
-      AXIL_MOSI.awprot <= (others => '0');
-      AXIL_MOSI.wdata <= (others => '0');
-      AXIL_MOSI.wstrb <= (others => '0');
-      AXIL_MOSI.araddr <= (others => '0');
-      AXIL_MOSI.arprot <= (others => '0');
+begin
+   AXIL_MOSI.awaddr <= (others => '0');
+   AXIL_MOSI.awprot <= (others => '0');
+   AXIL_MOSI.wdata <= (others => '0');
+   AXIL_MOSI.wstrb <= (others => '0');
+   AXIL_MOSI.araddr <= (others => '0');
+   AXIL_MOSI.arprot <= (others => '0');
 
-      AXIL_MOSI.awvalid <= '0';
-      AXIL_MOSI.wvalid <= '0';
-      AXIL_MOSI.bready <= '0';
-      AXIL_MOSI.arvalid <= '0';
-      AXIL_MOSI.rready <= '0';
-	  
-	  
-      wait until rstn_i = '1';
-
-      wait until rising_edge(clk100_i);
-      -- Soft trig
-      wait until rising_edge(clk100_i);
+   AXIL_MOSI.awvalid <= '0';
+   AXIL_MOSI.wvalid <= '0';
+   AXIL_MOSI.bready <= '0';
+   AXIL_MOSI.arvalid <= '0';
+   AXIL_MOSI.rready <= '0';
    
-      write_axi_lite (clk100_i, x"00000000", resize(std_logic_vector(ANYEDGE), 32), AXIL_MISO,  AXIL_MOSI);
-      wait until rising_edge(clk100_i);
-
-	   write_axi_lite (clk100_i, x"00000004", x"00000000", AXIL_MISO,  AXIL_MOSI);   -- delay
-      wait until rising_edge(clk100_i);
-	  
-	   write_axi_lite (clk100_i, x"00000008", x"0000000A", AXIL_MISO,  AXIL_MOSI);   -- fame count
-      wait until rising_edge(clk100_i);
-
-	   write_axi_lite (clk100_i, x"0000000C", x"00000000", AXIL_MISO,  AXIL_MOSI);   -- trig source
-      wait until rising_edge(clk100_i);
-
-      wait for 30 ns;
-      
-	   write_axi_lite (clk100_i, x"00000010", x"00000001", AXIL_MISO,  AXIL_MOSI);   -- trig
-      wait until rising_edge(clk100_i);
+   Trig_HW <= '0';
   
-      wait for 800 ns;
-      
-      write_axi_lite (clk100_i, x"00000000", resize(std_logic_vector(RISINGEDGE), 32), AXIL_MISO,  AXIL_MOSI);
-      wait until rising_edge(clk100_i);
+   wait until rstn_i = '1';
 
-	   write_axi_lite (clk100_i, x"00000004", x"0000000A", AXIL_MISO,  AXIL_MOSI);   -- delay
-      wait until rising_edge(clk100_i);
-	  
-	   write_axi_lite (clk100_i, x"00000008", x"0000000A", AXIL_MISO,  AXIL_MOSI);   -- fame count
-      wait until rising_edge(clk100_i);
+   wait until rising_edge(clk100_i);
+   
+   wait until rising_edge(clk100_i);
+   
+   ------------------------------------------
+   -- AXI functions
+   ------------------------------------------
+   --procedure write_axi_lite (signal Clk : in std_logic; Addr : in std_logic_vector(31 downto 0); Value : in std_logic_vector(31 downto 0);signal  miso : in  t_axi4_lite_miso;signal  mosi : out t_axi4_lite_mosi);
 
-	   write_axi_lite (clk100_i, x"0000000C", x"00000001", AXIL_MISO,  AXIL_MOSI);   -- trig source
-      wait until rising_edge(clk100_i);
+   write_axi_lite (clk100_i, x"00000000", resize(std_logic_vector(RISINGEDGE), 32), AXIL_MISO,  AXIL_MOSI);
+   wait until rising_edge(clk100_i);
 
-      wait for 30 ns;
-      
-	   write_axi_lite (clk100_i, x"00000010", x"00000001", AXIL_MISO,  AXIL_MOSI);   -- trig
-      wait until rising_edge(clk100_i);
+   write_axi_lite (clk100_i, x"00000004", x"00000010", AXIL_MISO,  AXIL_MOSI);   -- delay
+   wait until rising_edge(clk100_i);
   
-      wait for 200 ns;
-      
-	   write_axi_lite (clk100_i, x"00000010", x"00000001", AXIL_MISO,  AXIL_MOSI);   -- trig
-      wait until rising_edge(clk100_i);
+   write_axi_lite (clk100_i, x"00000008", x"00000004", AXIL_MISO,  AXIL_MOSI);   -- frame count
+   wait until rising_edge(clk100_i);
 
-      report "FCR written"; 
+   write_axi_lite (clk100_i, x"0000000C", resize("0" & FLAG_TRIG_SOFTWARE, 32), AXIL_MISO,  AXIL_MOSI);   -- trig source
+   wait until rising_edge(clk100_i);
 
-      report "END OF SIMULATION" 
-      severity error;
+   wait for 30 ns;
+   
+   write_axi_lite (clk100_i, x"00000010", x"00000001", AXIL_MISO,  AXIL_MOSI);   -- trig
+   wait until rising_edge(clk100_i);
+
+   wait for 500 ns;
+   
+   write_axi_lite (clk100_i, x"00000000", resize(std_logic_vector(LEVELHIGH), 32), AXIL_MISO,  AXIL_MOSI);
+   wait until rising_edge(clk100_i);
+
+   write_axi_lite (clk100_i, x"00000004", x"17D78400", AXIL_MISO,  AXIL_MOSI);   -- delay
+   wait until rising_edge(clk100_i);
+  
+   write_axi_lite (clk100_i, x"00000008", x"00000000", AXIL_MISO,  AXIL_MOSI);   -- frame count
+   wait until rising_edge(clk100_i);
+
+   write_axi_lite (clk100_i, x"0000000C", resize("0" & FLAG_TRIG_HARDWARE, 32), AXIL_MISO,  AXIL_MOSI);   -- trig source
+   wait until rising_edge(clk100_i);
+
+   wait for 30 ns;
+   
+   Trig_HW <= '1';
+   wait for 200 ns;
+   Trig_HW <= '0';
+   
+   wait for 4000000 us;
+
+   report "END OF SIMULATION" 
+   severity error;
 end process ublaze_stim;
 
  
