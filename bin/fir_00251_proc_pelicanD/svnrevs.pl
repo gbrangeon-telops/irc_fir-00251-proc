102b4c154f63bbf51f20de8a9674cb69a5e5c001
dalain-telpos <denis.alain@telops.com>
2017-05-25 17:07:58 +0000

    Release 2.1.x.0
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@20474 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_pelicanD/svnrevs.pl
+++ bin/fir_00251_proc_pelicanD/svnrevs.pl
@@ -1,5 +1,5 @@
-our $rel_proc_hw_rev = 19661;
-our $rel_proc_sw_rev = 20119;
-our $rel_proc_boot_rev = 19661;
-our $rel_proc_common_rev = 20069;
+our $rel_proc_hw_rev = 20472;
+our $rel_proc_sw_rev = 20472;
+our $rel_proc_boot_rev = 20472;
+our $rel_proc_common_rev = 20451;
 1;
