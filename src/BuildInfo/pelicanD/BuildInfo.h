4d0f5acd51833254f93f5e9547cb45e3643627b3
odionne-telpos <olivier.dionne@telops.com>
2017-12-20 21:29:58 +0000

    Release 2.3.x.0
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@21278 3e98269d-ff51-e449-abca-491a6567d830

--- src/BuildInfo/pelicanD/BuildInfo.h
+++ src/BuildInfo/pelicanD/BuildInfo.h
@@ -1,8 +1,8 @@
 #ifndef BUILDINFO_H
 #define BUILDINFO_H
 
-#define SVN_HARDWARE_REV      -20804
-#define SVN_SOFTWARE_REV      -21058
+#define SVN_HARDWARE_REV      21277
+#define SVN_SOFTWARE_REV      21277
 #define SVN_BOOTLOADER_REV    21058
 #define SVN_COMMON_REV        20950
 
