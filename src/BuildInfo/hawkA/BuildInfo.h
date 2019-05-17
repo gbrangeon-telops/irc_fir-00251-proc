dd92898721503d7a984caf9a32f43a88b41caf30
elarouche-telpos <eric.larouche@telops.com>
2019-05-17 16:23:04 +0000

    Pre-release 3.0.x.0
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@23569 3e98269d-ff51-e449-abca-491a6567d830

--- src/BuildInfo/hawkA/BuildInfo.h
+++ src/BuildInfo/hawkA/BuildInfo.h
@@ -3,10 +3,10 @@
 
 #ifdef ARCH_FPGA_160
 
-#define SVN_HARDWARE_REV      23419
-#define SVN_SOFTWARE_REV      23419
-#define SVN_BOOTLOADER_REV    23419
-#define SVN_COMMON_REV        23540
+#define SVN_HARDWARE_REV      -23419
+#define SVN_SOFTWARE_REV      -23419
+#define SVN_BOOTLOADER_REV    -23419
+#define SVN_COMMON_REV        23551
 
 #define SVN_UNCOMMITTED_CHANGES  ((SVN_HARDWARE_REV < 0) || (SVN_SOFTWARE_REV < 0) || (SVN_BOOTLOADER_REV < 0) || (SVN_COMMON_REV < 0))
 
@@ -14,7 +14,7 @@
 #warning Uncommitted changes detected.
 #endif
 
-#define HARDWARE_MISMATCH (1)
+#define HARDWARE_MISMATCH (0)
 
 #if HARDWARE_MISMATCH
 #error D:\Telops\FIR-00251-Proc\sdk\fir_00251_proc_hawkA\hw_platform_160\system.hdf does not match D:\Telops\FIR-00251-Proc\sdk\fir_00251_proc_hawkA\fir_00251_proc_hawkA_160.hdf
@@ -25,7 +25,7 @@
 #define SVN_HARDWARE_REV      23419
 #define SVN_SOFTWARE_REV      23419
 #define SVN_BOOTLOADER_REV    23419
-#define SVN_COMMON_REV        23540
+#define SVN_COMMON_REV        23551
 
 #define SVN_UNCOMMITTED_CHANGES  ((SVN_HARDWARE_REV < 0) || (SVN_SOFTWARE_REV < 0) || (SVN_BOOTLOADER_REV < 0) || (SVN_COMMON_REV < 0))
 
@@ -33,7 +33,7 @@
 #warning Uncommitted changes detected.
 #endif
 
-#define HARDWARE_MISMATCH (1)
+#define HARDWARE_MISMATCH (0)
 
 #if HARDWARE_MISMATCH
 #error D:\Telops\FIR-00251-Proc\sdk\fir_00251_proc_hawkA\hw_platform_325\system.hdf does not match D:\Telops\FIR-00251-Proc\sdk\fir_00251_proc_hawkA\fir_00251_proc_hawkA_325.hdf
