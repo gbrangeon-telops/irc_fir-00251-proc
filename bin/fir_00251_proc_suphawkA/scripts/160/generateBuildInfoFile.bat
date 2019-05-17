7d497b7ab5fea114f04946594572e61d083fcdcc
elarouche-telpos <eric.larouche@telops.com>
2019-05-17 16:18:23 +0000

    correction au script generationBuildInfoFile pour SuperHawk
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@23568 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_suphawkA/scripts/160/generateBuildInfoFile.bat
+++ bin/fir_00251_proc_suphawkA/scripts/160/generateBuildInfoFile.bat
@@ -1,3 +1,3 @@
 @echo off
 call D:\Telops\FIR-00251-Proc\bin\scripts\setEnvironment.bat suphawkA 160
-call %scriptsDir%\generateBuildInfoFile.bat hawkA
+call %scriptsDir%\generateBuildInfoFile.bat suphawkA
