ea4bc45e7b033ae57ef3b54f1d4a1e86d5936a4e
odionne-telpos <olivier.dionne@telops.com>
2019-07-15 15:27:43 +0000

    correction script suphawkA
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@23944 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_suphawkA/scripts/325/verifyRelease.bat
+++ bin/fir_00251_proc_suphawkA/scripts/325/verifyRelease.bat
@@ -1,3 +1,3 @@
 @echo off
-call D:\Telops\FIR-00251-Proc\bin\scripts\setEnvironment.bat supsuphawkA 325
+call D:\Telops\FIR-00251-Proc\bin\scripts\setEnvironment.bat suphawkA 325
 call %scriptsDir%\verifyRelease.bat
