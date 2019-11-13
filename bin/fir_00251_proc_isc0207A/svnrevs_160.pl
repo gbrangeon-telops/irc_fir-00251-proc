a59ea546d641663f41f9306ab9663f0e2ad0419a
pcouture-telpos <philippe.couture@telops.com>
2019-11-13 17:48:46 +0000

    Release 3.0.1.1
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@24454 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_isc0207A/svnrevs_160.pl
+++ bin/fir_00251_proc_isc0207A/svnrevs_160.pl
@@ -1,5 +1,5 @@
-our $rel_proc_hw_rev = 23593;
-our $rel_proc_sw_rev = 23593;
-our $rel_proc_boot_rev = 23419;
-our $rel_proc_common_rev = 23582;
+our $rel_proc_hw_rev = 24453;
+our $rel_proc_sw_rev = 24453;
+our $rel_proc_boot_rev = 24453;
+our $rel_proc_common_rev = 24443;
 1;
