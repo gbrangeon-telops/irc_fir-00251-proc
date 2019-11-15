5ccf24025b55c8a03e0b5eaf612996db34b91733
elarouche-telpos <eric.larouche@telops.com>
2019-11-15 19:47:15 +0000

    Relese 3.0.x.2
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@24477 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_isc0207A_3k/svnrevs_325.pl
+++ bin/fir_00251_proc_isc0207A_3k/svnrevs_325.pl
@@ -1,5 +1,5 @@
-our $rel_proc_hw_rev = 23593;
-our $rel_proc_sw_rev = 23593;
-our $rel_proc_boot_rev = 23569;
-our $rel_proc_common_rev = 23582;
+our $rel_proc_hw_rev = 24475;
+our $rel_proc_sw_rev = 24475;
+our $rel_proc_boot_rev = 24475;
+our $rel_proc_common_rev = 24443;
 1;
