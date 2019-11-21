cfc08213d46fb0b37daf1b7ca05b14daaa56b102
odionne-telpos <olivier.dionne@telops.com>
2019-11-21 19:47:31 +0000

    suphawkA compilé pour FPGA 160 par défaut
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@24503 3e98269d-ff51-e449-abca-491a6567d830

--- scripts/Build_all_project.tcl
+++ scripts/Build_all_project.tcl
@@ -50,8 +50,8 @@ lappend detector_list {scorpiomwD160}
 # lappend detector_list {startup160}
 # lappend detector_list {startup325}
 
-#lappend detector_list {suphawkA160}
-lappend detector_list {suphawkA325}
+lappend detector_list {suphawkA160}
+#lappend detector_list {suphawkA325}
 
 
 #DO NOT MODIFY BEYOND THIS POINT
