df33ff5035133eb29e73cd81268349fe48b0c0ae
odionne-telpos <olivier.dionne@telops.com>
2019-11-22 16:19:31 +0000

    XML 12.7.1
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@24509 3e98269d-ff51-e449-abca-491a6567d830

--- src/sw/GC_Registers.h
+++ src/sw/GC_Registers.h
@@ -59,10 +59,10 @@ extern uint8_t gGC_ProprietaryFeatureKeyIsValid;
 
 /* AUTO-CODE BEGIN */
 // Auto-generated GeniCam library.
-// Generated from XML camera definition file version 12.7.0
+// Generated from XML camera definition file version 12.7.1
 // using generateGenICamCLib.m Matlab script.
 
-#if ((GC_XMLMAJORVERSION != 12) || (GC_XMLMINORVERSION != 7) || (GC_XMLSUBMINORVERSION != 0))
+#if ((GC_XMLMAJORVERSION != 12) || (GC_XMLMINORVERSION != 7) || (GC_XMLSUBMINORVERSION != 1))
 #error "XML version mismatch."
 #endif
 
