96e6eb0c95cd98f8873cd1f824bcbdf9ec65bc83
odionne-telpos <olivier.dionne@telops.com>
2019-12-03 19:30:40 +0000

    Release actuel
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@24553 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_hawkA/svnrevs_160.pl
+++ bin/fir_00251_proc_hawkA/svnrevs_160.pl
@@ -1,5 +1,5 @@
-our $rel_proc_hw_rev = 24516;
-our $rel_proc_sw_rev = 24516;
-our $rel_proc_boot_rev = 24516;
-our $rel_proc_common_rev = 24509;
+our $rel_proc_hw_rev = 24550;
+our $rel_proc_sw_rev = 24550;
+our $rel_proc_boot_rev = 24550;
+our $rel_proc_common_rev = 24523;
 1;
