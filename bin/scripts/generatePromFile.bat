130f288776acdec6a43fc9fc51363fdc05afd676
odionne-telpos <olivier.dionne@telops.com>
2019-12-03 22:42:45 +0000

    correction
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@24557 3e98269d-ff51-e449-abca-491a6567d830

--- bin/scripts/generatePromFile.bat
+++ bin/scripts/generatePromFile.bat
@@ -24,15 +24,15 @@ if errorlevel 1 (
 
 rem Generate SREC image file
 %x_mb-objcopy% -O srec --srec-forceS3 --srec-len=16 %elfFile% %binDir%\tempS3_%fpgaSize%.srec
-%x_xilperl% %scriptsDir%\setSRecordS0.pl -i %binDir%\tempS3_%fpgaSize%.srec -t %baseName% -o tempS0_%fpgaSize%.srec
+%x_xilperl% %scriptsDir%\setSRecordS0.pl -i %binDir%\tempS3_%fpgaSize%.srec -t %baseName% -o %binDir%\tempS0_%fpgaSize%.srec
 
 rem Generate PROM file
-rem %x_promgen% -w -p mcs -spi -c FF -o "%binDir%\prom\%baseName%_%fpgaSize%.mcs" -s 16384 -u 0 %binDir%\download_%fpgaSize%.bit -data_file up 670000 "%releaseFile%" -data_file up 680000 "tempS0_%fpgaSize%.srec" > %binDir%\_promgen_%fpgaSize%.log
+rem %x_promgen% -w -p mcs -spi -c FF -o "%binDir%\prom\%baseName%_%fpgaSize%.mcs" -s 16384 -u 0 %binDir%\download_%fpgaSize%.bit -data_file up 670000 "%releaseFile%" -data_file up 680000 "%binDir%\tempS0_%fpgaSize%.srec" > %binDir%\_promgen_%fpgaSize%.log
 
 IF [%fpgaSize%]==[160] (
-%x_promgen% -w -p mcs -spi -c FF -o "%binDir%\prom\%baseName%_%fpgaSize%.mcs" -s 16384 -u 0 %binDir%\download_%fpgaSize%.bit -data_file up 670000 "%releaseFile%" -data_file up 680000 "tempS0_%fpgaSize%.srec" > %binDir%\_promgen_%fpgaSize%.log
+%x_promgen% -w -p mcs -spi -c FF -o "%binDir%\prom\%baseName%_%fpgaSize%.mcs" -s 16384 -u 0 %binDir%\download_%fpgaSize%.bit -data_file up 670000 "%releaseFile%" -data_file up 680000 "%binDir%\tempS0_%fpgaSize%.srec" > %binDir%\_promgen_%fpgaSize%.log
  ) ELSE ( 
-%x_promgen% -w -p mcs -spi -c FF -o "%binDir%\prom\%baseName%_%fpgaSize%.mcs" -s 16384 -u 0 %binDir%\download_%fpgaSize%.bit -data_file up AF0000 "%releaseFile%" -data_file up B00000 "tempS0_%fpgaSize%.srec" > %binDir%\_promgen_%fpgaSize%.log
+%x_promgen% -w -p mcs -spi -c FF -o "%binDir%\prom\%baseName%_%fpgaSize%.mcs" -s 16384 -u 0 %binDir%\download_%fpgaSize%.bit -data_file up AF0000 "%releaseFile%" -data_file up B00000 "%binDir%\tempS0_%fpgaSize%.srec" > %binDir%\_promgen_%fpgaSize%.log
  )
 if errorlevel 1 (
 	echo PROM file generation failed!
