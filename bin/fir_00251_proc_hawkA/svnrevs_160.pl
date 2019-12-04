470491f510c6f7dd012f9c041544cc1919f44557
odionne-telpos <olivier.dionne@telops.com>
2019-12-04 14:12:02 +0000

    Reverse merged revision(s) 24556-24555, 24553, 24550 from FIR-00251-Proc/trunk: Pre-release nouveau Release nouveau Release actuel Pre-release actuel
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@24560 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_hawkA/svnrevs_160.pl
+++ bin/fir_00251_proc_hawkA/svnrevs_160.pl
@@ -1,5 +1,5 @@
-our $rel_proc_hw_rev = 24534;
-our $rel_proc_sw_rev = 24555;
-our $rel_proc_boot_rev = 24555;
-our $rel_proc_common_rev = 24523;
+our $rel_proc_hw_rev = 24516;
+our $rel_proc_sw_rev = 24516;
+our $rel_proc_boot_rev = 24516;
+our $rel_proc_common_rev = 24509;
 1;
