ce4a55429fcd3ec5a9918cd7b773eaf1ec47b08c
pcouture-telpos <philippe.couture@telops.com>
2020-02-03 17:15:05 +0000

    Release 3.1.x.3
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@24860 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_isc0804A/svnrevs_325.pl
+++ bin/fir_00251_proc_isc0804A/svnrevs_325.pl
@@ -1,5 +1,5 @@
-our $rel_proc_hw_rev = 24534;
-our $rel_proc_sw_rev = 24742;
-our $rel_proc_boot_rev = 24725;
-our $rel_proc_common_rev = 24711;
+our $rel_proc_hw_rev = 24841;
+our $rel_proc_sw_rev = 24859;
+our $rel_proc_boot_rev = 24859;
+our $rel_proc_common_rev = 24839;
 1;
