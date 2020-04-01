58df8318390ac741cc166990c79a7c001b83b738
odionne-telpos <olivier.dionne@telops.com>
2020-04-01 20:01:19 +0000

    merge branch 2020-02-25 - hawkA NDF
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@25108 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_hawkA/svnrevs_160.pl
+++ bin/fir_00251_proc_hawkA/svnrevs_160.pl
@@ -1,5 +1,5 @@
 our $rel_proc_hw_rev = 24841;
-our $rel_proc_sw_rev = 24842;
-our $rel_proc_boot_rev = 24795;
+our $rel_proc_sw_rev = 25017;
+our $rel_proc_boot_rev = 25016;
 our $rel_proc_common_rev = 24839;
 1;
