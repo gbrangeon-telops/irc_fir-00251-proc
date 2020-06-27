2f2821abe76f131547e883d95692b43dd020ee74
odionne-telpos <olivier.dionne@telops.com>
2020-06-27 21:42:50 +0000

    Pre-release 4.1.x.1
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@25381 3e98269d-ff51-e449-abca-491a6567d830

--- src/BuildInfo/scorpiomwA/BuildInfo.h
+++ src/BuildInfo/scorpiomwA/BuildInfo.h
@@ -3,8 +3,8 @@
 
 #ifdef ARCH_FPGA_160
 
-#define SVN_HARDWARE_REV      25165
-#define SVN_SOFTWARE_REV      25305
+#define SVN_HARDWARE_REV      25380
+#define SVN_SOFTWARE_REV      25306
 #define SVN_BOOTLOADER_REV    25086
 #define SVN_COMMON_REV        25290
 
