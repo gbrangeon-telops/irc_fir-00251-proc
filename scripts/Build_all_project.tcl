c20b3785681c6141bf0ae60a5b3cbf5b743dfe1d
pcouture-telpos <philippe.couture@telops.com>
2020-11-23 13:36:03 +0000

    Build HW
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@26009 3e98269d-ff51-e449-abca-491a6567d830

--- scripts/Build_all_project.tcl
+++ scripts/Build_all_project.tcl
@@ -7,7 +7,7 @@ set release 1
 #Select Detector to build
 
 #lappend detector_list {blackbird1280D160}
-lappend detector_list {blackbird1280D325}
+#lappend detector_list {blackbird1280D325}
 
 lappend detector_list {hawkA160}
 #lappend detector_list {hawkA325}
@@ -55,7 +55,7 @@ lappend detector_list {suphawkA160}
 #lappend detector_list {suphawkA325}
 
 #lappend detector_list {xro3503A160}
-lappend detector_list {xro3503A325}
+#lappend detector_list {xro3503A325}
 
 #DO NOT MODIFY BEYOND THIS POINT
 
