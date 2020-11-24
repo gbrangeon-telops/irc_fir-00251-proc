690b60437564741012e482e0585c28c59227d354
enofodjie-telpos <edem.nofodjie@telops.com>
2020-11-24 22:29:36 +0000

    bb19920 : chantier cropping
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@26031 3e98269d-ff51-e449-abca-491a6567d830

---
+++ src/FPA/scd_proxy2/HDL/scd_proxy2_pix_pos.vhd
@@ -0,0 +1,108 @@
+------------------------------------------------------------------
+--!   @file : scd_proxy2_pix_pos
+--!   @brief
+--!   @details
+--!
+--!   $Rev$
+--!   $Author$
+--!   $Date$
+--!   $Id$
+--!   $URL$
+------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.numeric_std.all;
+use work.FPA_Define.all;
+use work.fpa_common_pkg.all;  
+
+entity scd_proxy2_pix_pos is
+   port(
+      ARESET         : in std_logic;
+      CLK            : in std_logic;
+      
+      FPA_INTF_CFG   : in fpa_intf_cfg_type;
+      
+      RX_MOSI        : in t_ll_ext_mosi72;
+      RX_MISO        : out t_ll_ext_miso;
+      
+      TX_MOSI        : out t_ll_ext_mosi72;
+      TX_MISO        : in t_ll_ext_miso;
+      PIX_POS        : out std_logic_vector(7 downto 0);
+      
+      ERR            : out  std_logic
+      );
+end scd_proxy2_pix_pos;
+
+
+
+architecture rtl of scd_proxy2_pix_pos is
+   
+   component sync_reset
+      port (
+         ARESET : in STD_LOGIC;
+         CLK : in STD_LOGIC;
+         SRESET : out STD_LOGIC := '1'
+         );
+   end component;
+   
+   signal sreset          : std_logic; 
+   signal pix_pos_i       : unsigned(PIX_POS'LENGTH-1 downto 0); 
+   signal err_i           : std_logic;
+   signal tx_mosi_i       : t_ll_ext_mosi72;
+   
+begin
+   
+   TX_MOSI <= tx_mosi_i;
+   RX_MISO <= TX_MISO;
+   
+   PIX_POS <= std_logic_vector(pix_pos_i);
+   
+   ERR <= err_i;
+   
+   
+   ------------------------------------------------------
+   -- Sync reset
+   ------------------------------------------------------
+   U1 : sync_reset
+   port map(
+      ARESET => ARESET,
+      CLK => CLK,
+      SRESET => sreset
+      );
+   
+   -----------------------------------------------------
+   -- numerotation des quads
+   -----------------------------------------------------    
+   U2: process(CLK)
+
+   begin  
+      if rising_edge(CLK) then 
+         if sreset = '1' then 
+            tx_mosi_i.dval <= '0';
+            pix_pos_i <= (others => '0'); 
+            err_i <= '0';  
+            
+         else 
+            
+            -- sortie des données 
+            tx_mosi_i  <=  RX_MOSI;
+            
+            -- sortie des numeros des quads
+            if RX_MOSI.DVAL = '1' then 
+               if RX_MOSI.SOL = '1' then 
+                  pix_pos_i <= to_unsigned(1, pix_pos_i'length);
+               else
+                  pix_pos_i <= pix_pos_i + 1;
+               end if;				  
+            end if;			
+            
+            -- erreur
+            err_i <= TX_MISO.BUSY and tx_mosi_i.dval;
+            
+         end if;           
+      end if;			
+   end process;
+   
+   
+   
+end rtl;
---
+++ src/FPA/scd_proxy2/HDL/scd_proxy2_pix_sel.vhd
@@ -0,0 +1,102 @@
+------------------------------------------------------------------
+--!   @file : scd_proxy2_pix_sel
+--!   @brief
+--!   @details
+--!
+--!   $Rev$
+--!   $Author$
+--!   $Date$
+--!   $Id$
+--!   $URL$
+------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.numeric_std.all;
+use work.FPA_Define.all;
+use work.fpa_common_pkg.all;
+
+entity scd_proxy2_pix_sel is
+   port(
+      ARESET         : in std_logic;
+      CLK            : in std_logic;
+      
+      CFG            : in line_area_type;
+      
+      RX_MOSI        : in t_ll_ext_mosi72;
+      RX_MISO        : out t_ll_ext_miso;
+      RX_POS         : in std_logic_vector(9 downto 0);
+      
+      TX_MOSI        : out t_ll_ext_mosi72;
+      TX_MISO        : in t_ll_ext_miso;
+      
+      ERR            : out std_logic
+      );
+end scd_proxy2_pix_sel;
+
+
+
+architecture rtl of scd_proxy2_pix_sel is
+   
+   component sync_reset
+      port (
+         ARESET : in STD_LOGIC;
+         CLK : in STD_LOGIC;
+         SRESET : out STD_LOGIC := '1'
+         );
+   end component;
+   
+   type tx_mosi_pipe_type is array (0 to 1) of t_ll_ext_mosi72;
+   
+   signal sreset              : std_logic; 
+   signal err_i               : std_logic;
+   signal tx_mosi_pipe        : tx_mosi_pipe_type;
+   signal sample_valid_set1   : std_logic;
+   signal sample_valid_set2   : std_logic;
+   
+begin
+   
+   TX_MOSI <= tx_mosi_pipe(1);   
+   ERR <= err_i;
+   RX_MISO <= TX_MISO;
+   
+   
+   ------------------------------------------------------
+   -- Sync reset
+   ------------------------------------------------------
+   U1 : sync_reset
+   port map(
+      ARESET => ARESET,
+      CLK => CLK,
+      SRESET => sreset
+      );
+   
+   ------------------------------------------------------
+   -- choix des données
+   ------------------------------------------------------
+   U2 : process(CLK)
+   begin
+      if rising_edge(CLK) then 
+         
+         tx_mosi_pipe(0) <= RX_MOSI;
+         
+         if  unsigned(RX_POS) >= to_integer(CFG.SOL_POSL) then
+            sample_valid_set1 <= RX_MOSI.DVAL;
+         else
+            sample_valid_set1 <= '0';
+         end if;
+         
+         if  unsigned(RX_POS) <= to_integer(CFG.SOL_POSL) then
+            sample_valid_set2 <= RX_MOSI.DVAL;
+         else
+            sample_valid_set2 <= '0';  
+         end if;
+         
+         tx_mosi_pipe(1) <= tx_mosi_pipe(0);
+         tx_mosi_pipe(1).dval <=sample_valid_set1 and sample_valid_set2;
+         
+      end if;   
+   end process;
+   
+end rtl;
