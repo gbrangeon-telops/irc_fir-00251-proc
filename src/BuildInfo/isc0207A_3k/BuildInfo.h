d804766d9861791c838771f0208c21d86008449c
pcouture-telpos <philippe.couture@telops.com>
2020-12-03 15:04:11 +0000

    Release 5.0.x.2
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@26052 3e98269d-ff51-e449-abca-491a6567d830

--- src/BuildInfo/isc0207A_3k/BuildInfo.h
+++ src/BuildInfo/isc0207A_3k/BuildInfo.h
@@ -23,7 +23,7 @@
 #elif defined(ARCH_FPGA_325)
 
 #define SVN_HARDWARE_REV      26009
-#define SVN_SOFTWARE_REV      26025
+#define SVN_SOFTWARE_REV      26051
 #define SVN_BOOTLOADER_REV    26009
 #define SVN_COMMON_REV        26004
 
