85db19850febae0055c8e832d0cbfd2ca452205d
elarouche-telpos <eric.larouche@telops.com>
2021-02-26 23:49:34 +0000

    Release 5.0.x.5
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@26271 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_scorpiomwA/svnrevs_160.pl
+++ bin/fir_00251_proc_scorpiomwA/svnrevs_160.pl
@@ -1,5 +1,5 @@
-our $rel_proc_hw_rev = 26243;
-our $rel_proc_sw_rev = 26244;
+our $rel_proc_hw_rev = 26263;
+our $rel_proc_sw_rev = 26270;
 our $rel_proc_boot_rev = 26009;
-our $rel_proc_common_rev = 26204;
+our $rel_proc_common_rev = 26253;
 1;
