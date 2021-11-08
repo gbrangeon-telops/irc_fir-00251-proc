412d5a44c4e3516cfceed007ff99980e55226014
pcouture-telpos <philippe.couture@telops.com>
2021-11-08 20:04:05 +0000

    Release 5.1.x.2
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@26940 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_herculesD/svnrevs_160.pl
+++ bin/fir_00251_proc_herculesD/svnrevs_160.pl
@@ -1,5 +1,5 @@
-our $rel_proc_hw_rev = 26769;
-our $rel_proc_sw_rev = 26780;
+our $rel_proc_hw_rev = 26938;
+our $rel_proc_sw_rev = 26939;
 our $rel_proc_boot_rev = 26769;
 our $rel_proc_common_rev = 26738;
 1;
