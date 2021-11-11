db7d8ba02afd3fbd5f852c8dd7a78e39cc076bcb
pcouture-telpos <philippe.couture@telops.com>
2021-11-11 16:24:43 +0000

    Release 5.1.x.2
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@26956 3e98269d-ff51-e449-abca-491a6567d830

--- src/BuildInfo/herculesD/BuildInfo.h
+++ src/BuildInfo/herculesD/BuildInfo.h
@@ -4,7 +4,7 @@
 #ifdef ARCH_FPGA_160
 
 #define SVN_HARDWARE_REV      26938
-#define SVN_SOFTWARE_REV      26940
+#define SVN_SOFTWARE_REV      26955
 #define SVN_BOOTLOADER_REV    26769
 #define SVN_COMMON_REV        26738
 
