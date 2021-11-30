fe12b219e15f42729712688fc4425ca61f27724c
pcouture-telpos <philippe.couture@telops.com>
2021-11-30 20:57:03 +0000

    Release 5.1.x.3
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@26992 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_blackbird1280D/svnrevs_325.pl
+++ bin/fir_00251_proc_blackbird1280D/svnrevs_325.pl
@@ -1,5 +1,5 @@
-our $rel_proc_hw_rev = 26769;
-our $rel_proc_sw_rev = 26780;
+our $rel_proc_hw_rev = 26990;
+our $rel_proc_sw_rev = 26991;
 our $rel_proc_boot_rev = 26769;
 our $rel_proc_common_rev = 26738;
 1;
--- src/BuildInfo/blackbird1280D/BuildInfo.h
+++ src/BuildInfo/blackbird1280D/BuildInfo.h
@@ -23,7 +23,7 @@
 #elif defined(ARCH_FPGA_325)
 
 #define SVN_HARDWARE_REV      26990
-#define SVN_SOFTWARE_REV      26781
+#define SVN_SOFTWARE_REV      26991
 #define SVN_BOOTLOADER_REV    26769
 #define SVN_COMMON_REV        26738
 
