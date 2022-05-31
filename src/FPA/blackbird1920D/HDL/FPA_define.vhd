1178d59fa4f659063001903968a9ccd63957cae1
pcouture-telpos <philippe.couture@telops.com>
2022-05-31 17:58:26 +0000

    test
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@27534 3e98269d-ff51-e449-abca-491a6567d830

--- src/FPA/blackbird1920D/HDL/FPA_define.vhd
+++ src/FPA/blackbird1920D/HDL/FPA_define.vhd
@@ -33,7 +33,7 @@ package FPA_define is
    constant DEFINE_FPA_INIT_CFG_NEEDED             : std_logic  := '0';     									-- pas besoin de config particulière au demarrage du PelicanD  
     
    constant DEFINE_INT_CLK_SOURCE_RATE_KHZ         : integer    := 70_000;           -- frequence de l'horloge source de laquelle est tirée celle de INT
-   constant DEFINE_ADC_QUAD_CLK_SOURCE_RATE_KHZ    : integer    := 70_000;           -- frequence de l'horloge source de laquelle est tirée celle de ADC_QUAD_CLK
+   constant DEFINE_ADC_QUAD_CLK_SOURCE_RATE_KHZ    : integer    := 70_000;           -- frequence de l'horloge source de laquelle est tirée celle de ADC_QUAD_CLK Test
    constant DEFINE_FPA_PCLK_RATE_KHZ               : integer    := 70_000;
    constant DEFINE_XTAL_CLK_RATE_HZ                : integer    := 70_000_000;           -- frequence de l'horloge XTAL pour le RS232
    
