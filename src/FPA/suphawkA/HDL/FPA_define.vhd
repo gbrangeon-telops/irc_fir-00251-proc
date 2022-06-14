fa57bd466ea2da7d9bab342fc04e76b2899a01af
elarouche-telpos <eric.larouche@telops.com>
2022-06-14 14:15:40 +0000

    merge de r27562: superhawkA: ne pas générer le module de correction électronique pour réduire les risques de bugs.
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@27572 3e98269d-ff51-e449-abca-491a6567d830

--- src/FPA/suphawkA/HDL/FPA_define.vhd
+++ src/FPA/suphawkA/HDL/FPA_define.vhd
@@ -53,8 +53,8 @@ package FPA_define is
    constant DEFINE_GENERATE_VPROC_CHAIN           : std_logic := '0';      -- diversité de canaux
    constant DEFINE_GENERATE_QUAD2_CHAIN           : std_logic := '1';      -- à '1' permet de generer la chaine de traitement pour le quad 2 puisqu'il faut 8 canaux
    constant DEFINE_FPA_LINE_SYNC_MODE             : boolean   := true;     -- utilisé dans le module afpa_real_data_gen pour signaler à TRUE qu'il faille se synchroniser sur chaque ligne et à false pour signaler qu'une synchro en debut de trame est suffisante ou s
-   constant DEFINE_GENERATE_ELCORR_CHAIN          : std_logic := '1';      -- on fait de la correction électronique
-   constant DEFINE_GENERATE_ELCORR_GAIN           : std_logic := '1';      -- on ne fait aucune correction de gain, mais juste l'offset au besoin
+   constant DEFINE_GENERATE_ELCORR_CHAIN          : std_logic := '0';      -- on fait de la correction électronique
+   constant DEFINE_GENERATE_ELCORR_GAIN           : std_logic := '0';      -- on ne fait aucune correction de gain, mais juste l'offset au besoin
    constant DEFINE_ELCORR_REF_DAC_SETUP_US        : integer   := 300_000;  -- en usec, le delai de stabilisation (analog setup)
    constant DEFINE_GENERATE_CROPPING_CHAIN        : std_logic := '0';      -- on ne fait pas de cropping
    
