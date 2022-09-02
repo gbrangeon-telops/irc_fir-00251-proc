e12335b26afcaf0fc7f28f8e875c9c2e341e29db
pcouture-telpos <philippe.couture@telops.com>
2022-09-02 17:42:09 +0000

    Release 5.3.x.4
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@27850 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_blackbird1920D/svnrevs_325.pl
+++ bin/fir_00251_proc_blackbird1920D/svnrevs_325.pl
@@ -1,5 +1,5 @@
-our $rel_proc_hw_rev = 27605;
-our $rel_proc_sw_rev = 27606;
-our $rel_proc_boot_rev = 27479;
-our $rel_proc_common_rev = 27602;
+our $rel_proc_hw_rev = 27840;
+our $rel_proc_sw_rev = 27849;
+our $rel_proc_boot_rev = 27817;
+our $rel_proc_common_rev = 27839;
 1;
--- src/BuildInfo/blackbird1920D/BuildInfo.h
+++ src/BuildInfo/blackbird1920D/BuildInfo.h
@@ -23,7 +23,7 @@
 #elif defined(ARCH_FPGA_325)
 
 #define SVN_HARDWARE_REV      27840
-#define SVN_SOFTWARE_REV      27608
+#define SVN_SOFTWARE_REV      27849
 #define SVN_BOOTLOADER_REV    27817
 #define SVN_COMMON_REV        27839
 
