8bb4a6dde1934276c3c5bda3b3354f3980923714
pcouture-telpos <philippe.couture@telops.com>
2022-11-03 18:58:37 +0000

    Pre-release 5.3.x.7
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@28080 3e98269d-ff51-e449-abca-491a6567d830

--- src/BuildInfo/startup_4DDR/BuildInfo.h
+++ src/BuildInfo/startup_4DDR/BuildInfo.h
@@ -3,9 +3,9 @@
 
 #ifdef ARCH_FPGA_160
 
-#define SVN_HARDWARE_REV      27605
-#define SVN_SOFTWARE_REV      27746
-#define SVN_BOOTLOADER_REV    27479
+#define SVN_HARDWARE_REV      28051
+#define SVN_SOFTWARE_REV      28051
+#define SVN_BOOTLOADER_REV    28051
 #define SVN_COMMON_REV        28051
 
 #define SVN_UNCOMMITTED_CHANGES  ((SVN_HARDWARE_REV < 0) || (SVN_SOFTWARE_REV < 0) || (SVN_BOOTLOADER_REV < 0) || (SVN_COMMON_REV < 0))
@@ -14,17 +14,17 @@
 #warning Uncommitted changes detected.
 #endif
 
-#define HARDWARE_MISMATCH (0)
+#define HARDWARE_MISMATCH (1)
 
 #if HARDWARE_MISMATCH
-#error D:\Telops\FIR-00251-Proc\sdk\fir_00251_proc_startup\hw_platform_160\system.hdf does not match D:\Telops\FIR-00251-Proc\sdk\fir_00251_proc_startup\fir_00251_proc_startup_160.hdf
+#error D:\Telops\FIR-00251-Proc\sdk\fir_00251_proc_startup_4DDR\hw_platform_160\system.hdf does not match D:\Telops\FIR-00251-Proc\sdk\fir_00251_proc_startup_4DDR\fir_00251_proc_startup_4DDR_160.hdf
 #endif
 
 #elif defined(ARCH_FPGA_325)
 
-#define SVN_HARDWARE_REV      27605
-#define SVN_SOFTWARE_REV      27746
-#define SVN_BOOTLOADER_REV    27479
+#define SVN_HARDWARE_REV      28053
+#define SVN_SOFTWARE_REV      28053
+#define SVN_BOOTLOADER_REV    28053
 #define SVN_COMMON_REV        28051
 
 #define SVN_UNCOMMITTED_CHANGES  ((SVN_HARDWARE_REV < 0) || (SVN_SOFTWARE_REV < 0) || (SVN_BOOTLOADER_REV < 0) || (SVN_COMMON_REV < 0))
@@ -36,7 +36,7 @@
 #define HARDWARE_MISMATCH (0)
 
 #if HARDWARE_MISMATCH
-#error D:\Telops\FIR-00251-Proc\sdk\fir_00251_proc_startup_4DDR\hw_platform_325\system.hdf does not match D:\Telops\FIR-00251-Proc\sdk\fir_00251_proc_startup_4DDR\fir_00251_proc_startup_325.hdf
+#error D:\Telops\FIR-00251-Proc\sdk\fir_00251_proc_startup_4DDR\hw_platform_325\system.hdf does not match D:\Telops\FIR-00251-Proc\sdk\fir_00251_proc_startup_4DDR\fir_00251_proc_startup_4DDR_325.hdf
 #endif
 
 #endif  // FPGA_ARCH Check
