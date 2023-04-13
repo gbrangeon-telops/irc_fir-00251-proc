d47691692cedb5607c6929316482d25fa714e6de
odionne-telpos <olivier.dionne@telops.com>
2023-04-13 21:47:02 +0000

    le script tcl doit spécifier la taille 325 puisque le 160 n'existe pas
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@28756 3e98269d-ff51-e449-abca-491a6567d830

--- sdk/build_all_sw.tcl
+++ sdk/build_all_sw.tcl
@@ -73,7 +73,7 @@ create_proc_sw startup
 build_proc_sw startup 160
 build_proc_sw startup 325
 
-create_proc_sw startup_4DDR
+create_proc_sw startup_4DDR 325
 build_proc_sw startup_4DDR 325
 
 create_proc_sw suphawkA
