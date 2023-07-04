3c1c00d701111b69ddabf2bc14b704cfb9685a5c
elarouche-telpos <eric.larouche@telops.com>
2023-07-04 20:15:19 +0000

    Release 5.5.x.2
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@28968 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_suphawkA/svnrevs_325.pl
+++ bin/fir_00251_proc_suphawkA/svnrevs_325.pl
@@ -1,5 +1,5 @@
-our $rel_proc_hw_rev = 28178;
-our $rel_proc_sw_rev = 28210;
-our $rel_proc_boot_rev = 27605;
-our $rel_proc_common_rev = 28179;
+our $rel_proc_hw_rev = 28956;
+our $rel_proc_sw_rev = 28967;
+our $rel_proc_boot_rev = 28956;
+our $rel_proc_common_rev = 28736;
 1;
--- src/BuildInfo/suphawkA/BuildInfo.h
+++ src/BuildInfo/suphawkA/BuildInfo.h
@@ -23,7 +23,7 @@
 #elif defined(ARCH_FPGA_325)
 
 #define SVN_HARDWARE_REV      28956
-#define SVN_SOFTWARE_REV      28211
+#define SVN_SOFTWARE_REV      28967
 #define SVN_BOOTLOADER_REV    28956
 #define SVN_COMMON_REV        28736
 
