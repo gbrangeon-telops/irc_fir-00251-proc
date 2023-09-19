04e0099216f77342d18312c0d42354da12b14ec5
elarouche-telpos <eric.larouche@telops.com>
2023-09-19 18:53:59 +0000

    isc0804A_2k: migration des scripts de bin
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@29191 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_isc0804A_2k/scripts/325/generatePromFile.bat
+++ bin/fir_00251_proc_isc0804A_2k/scripts/325/generatePromFile.bat
@@ -1,2 +1,7 @@
+@echo off
+
 call D:\Telops\FIR-00251-Proc\bin\scripts\setEnvironment.bat isc0804A_2k 325
-call %scriptsDir%\generatePromFile.bat
+
+%xDir%\Vivado\2018.3\bin\vivado -mode batch -source generatePromFile325.tcl
+
+cmd /k
