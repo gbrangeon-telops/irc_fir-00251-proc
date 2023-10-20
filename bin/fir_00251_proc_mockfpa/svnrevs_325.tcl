5718cefe614d17eafb79467e144465d1a9483161
elarouche-telpos <eric.larouche@telops.com>
2023-10-20 15:41:20 +0000

    build sw (isc0804A, mockfpa)
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@29311 3e98269d-ff51-e449-abca-491a6567d830

--- bin/fir_00251_proc_mockfpa/svnrevs_325.tcl
+++ bin/fir_00251_proc_mockfpa/svnrevs_325.tcl
@@ -1,5 +1,4 @@
-set rel_proc_hw_rev 28835
-set rel_proc_sw_rev 28617
-set rel_proc_boot_rev 28835
-set rel_proc_common_rev 28958
-1 
+set rel_proc_hw_rev 29308
+set rel_proc_sw_rev 29295
+set rel_proc_boot_rev 29274
+set rel_proc_common_rev 29303
