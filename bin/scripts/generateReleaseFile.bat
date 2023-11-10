548e729a582718dedab63cf3f8e50487a08efd9a
apullara-telpos <alexis.pullara@telops.com>
2023-11-10 09:04:23 +0000

    Correction maj firmware version
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@29396 3e98269d-ff51-e449-abca-491a6567d830

--- bin/scripts/generateReleaseFile.bat
+++ bin/scripts/generateReleaseFile.bat
@@ -5,6 +5,9 @@ rem Clean up
 del %releaseFile%
 del %releaseLogFile%
 
+REM Call setEnvironment to get the last firmwareVersion values
+call D:\Telops\FIR-00251-Proc\bin\scripts\setEnvironment.bat %sensorName% %fpgaSize%
+
 call %x_xsct% %scriptsDir%\generateReleaseFile.tcl -fa %firmwareVersionMajor% -fi %firmwareVersionMinor% -fs ^
 %firmwareVersionSubMinor% -fb %firmwareVersionBuild% -p %revFile% -o %outputRevFile% -s1 %storageRevFile1% -s2 %storageRevFile2% -r %releaseFile% -l %releaseLogFile%
 if errorlevel 1 (
