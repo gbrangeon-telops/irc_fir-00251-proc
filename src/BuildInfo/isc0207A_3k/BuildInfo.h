85de6509dc69a7ffb50a3b78a286ebecd1de76e8
pcouture-telpos <philippe.couture@telops.com>
2023-11-21 13:27:34 +0000

    Release 6.0.x.1
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@29514 3e98269d-ff51-e449-abca-491a6567d830

--- src/BuildInfo/isc0207A_3k/BuildInfo.h
+++ src/BuildInfo/isc0207A_3k/BuildInfo.h
@@ -28,7 +28,7 @@
 #elif defined(ARCH_FPGA_325)
  
 #define SVN_HARDWARE_REV 29512
-#define SVN_SOFTWARE_REV      29289
+#define SVN_SOFTWARE_REV      29513
  #define SVN_BOOTLOADER_REV   29272
  #define SVN_COMMON_REV      29379
  
