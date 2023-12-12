adcf326d02b08f46370a454cb0bca306951a277a
apullara-telpos <alexis.pullara@telops.com>
2023-12-12 10:01:23 +0000

    "Release 6.0.x.2"
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@29625 3e98269d-ff51-e449-abca-491a6567d830

--- src/BuildInfo/isc0207A/BuildInfo.h
+++ src/BuildInfo/isc0207A/BuildInfo.h
@@ -15,7 +15,7 @@
 #ifdef ARCH_FPGA_160
  
 #define SVN_HARDWARE_REV 29611
-#define SVN_SOFTWARE_REV      29525
+#define SVN_SOFTWARE_REV      29624
  #define SVN_BOOTLOADER_REV   29272
  #define SVN_COMMON_REV      29542
  
