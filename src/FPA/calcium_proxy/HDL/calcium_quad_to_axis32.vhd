5ed692daa3ae8ddd2ca2196d0a5e19b5ef2232fc
odionne-telpos <olivier.dionne@telops.com>
2024-01-11 23:57:38 +0000

    calcium: ajout de la compression (simple troncature pour l'instant)
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@29735 3e98269d-ff51-e449-abca-491a6567d830

--- src/FPA/calcium_proxy/HDL/calcium_quad_to_axis32.vhd
+++ src/FPA/calcium_proxy/HDL/calcium_quad_to_axis32.vhd
@@ -25,17 +25,8 @@ entity calcium_quad_to_axis32 is
       
       RX_QUAD_DATA   : in calcium_quad_data_type;
       
-      TX0_MOSI       : out t_axi4_stream_mosi32;
-      TX0_MISO       : in t_axi4_stream_miso;
-      
-      TX1_MOSI       : out t_axi4_stream_mosi32;
-      TX1_MISO       : in t_axi4_stream_miso;
-      
-      TX2_MOSI       : out t_axi4_stream_mosi32;
-      TX2_MISO       : in t_axi4_stream_miso;
-      
-      TX3_MOSI       : out t_axi4_stream_mosi32;
-      TX3_MISO       : in t_axi4_stream_miso;
+      TX_MOSI_ARY    : out t_axis4_mosi32_a(1 to 4);
+      TX_MISO_ARY    : in t_axis4_miso_a(1 to 4);
       
       ERROR          : out std_logic  
    );
@@ -62,41 +53,16 @@ begin
    ERROR <= error_o;
    
    -- Output data mapping
-   TX0_MOSI.TDATA    <= resize(RX_QUAD_DATA.PIX_DATA(1), TX0_MOSI.TDATA'length);
-   TX0_MOSI.TVALID   <= RX_QUAD_DATA.AOI_DVAL;     -- only pixel data
-   TX0_MOSI.TSTRB    <= (others => '1');
-   TX0_MOSI.TKEEP    <= (others => '1');
-   TX0_MOSI.TLAST    <= RX_QUAD_DATA.AOI_LAST;
-   TX0_MOSI.TID      <= (others => '0');
-   TX0_MOSI.TDEST    <= (others => '0');
-   TX0_MOSI.TUSER    <= (others => '0');
-   
-   TX1_MOSI.TDATA    <= resize(RX_QUAD_DATA.PIX_DATA(2), TX1_MOSI.TDATA'length);
-   TX1_MOSI.TVALID   <= RX_QUAD_DATA.AOI_DVAL;     -- only pixel data
-   TX1_MOSI.TSTRB    <= (others => '1');
-   TX1_MOSI.TKEEP    <= (others => '1');
-   TX1_MOSI.TLAST    <= RX_QUAD_DATA.AOI_LAST;
-   TX1_MOSI.TID      <= (others => '0');
-   TX1_MOSI.TDEST    <= (others => '0');
-   TX1_MOSI.TUSER    <= (others => '0');
-   
-   TX2_MOSI.TDATA    <= resize(RX_QUAD_DATA.PIX_DATA(3), TX2_MOSI.TDATA'length);
-   TX2_MOSI.TVALID   <= RX_QUAD_DATA.AOI_DVAL;     -- only pixel data
-   TX2_MOSI.TSTRB    <= (others => '1');
-   TX2_MOSI.TKEEP    <= (others => '1');
-   TX2_MOSI.TLAST    <= RX_QUAD_DATA.AOI_LAST;
-   TX2_MOSI.TID      <= (others => '0');
-   TX2_MOSI.TDEST    <= (others => '0');
-   TX2_MOSI.TUSER    <= (others => '0');
-   
-   TX3_MOSI.TDATA    <= resize(RX_QUAD_DATA.PIX_DATA(4), TX3_MOSI.TDATA'length);
-   TX3_MOSI.TVALID   <= RX_QUAD_DATA.AOI_DVAL;     -- only pixel data
-   TX3_MOSI.TSTRB    <= (others => '1');
-   TX3_MOSI.TKEEP    <= (others => '1');
-   TX3_MOSI.TLAST    <= RX_QUAD_DATA.AOI_LAST;
-   TX3_MOSI.TID      <= (others => '0');
-   TX3_MOSI.TDEST    <= (others => '0');
-   TX3_MOSI.TUSER    <= (others => '0');
+   Output_gen : for ii in 1 to 4 generate
+      TX_MOSI_ARY(ii).TDATA    <= resize(RX_QUAD_DATA.PIX_DATA(ii), TX_MOSI_ARY(ii).TDATA'length);
+      TX_MOSI_ARY(ii).TVALID   <= RX_QUAD_DATA.AOI_DVAL;     -- only pixel data
+      TX_MOSI_ARY(ii).TSTRB    <= (others => '1');
+      TX_MOSI_ARY(ii).TKEEP    <= (others => '1');
+      TX_MOSI_ARY(ii).TLAST    <= RX_QUAD_DATA.AOI_LAST;
+      TX_MOSI_ARY(ii).TID      <= (others => '0');
+      TX_MOSI_ARY(ii).TDEST    <= (others => '0');
+      TX_MOSI_ARY(ii).TUSER    <= (others => '0');
+   end generate;
    
    --------------------------------------------------
    -- Reset
@@ -119,7 +85,7 @@ begin
          else             
             
             -- Throughput error: this module does not support when tready is low. This error is latched
-            if RX_QUAD_DATA.AOI_DVAL = '1' and (TX0_MISO.TREADY = '0' or TX1_MISO.TREADY = '0' or TX2_MISO.TREADY = '0' or TX3_MISO.TREADY = '0') then
+            if RX_QUAD_DATA.AOI_DVAL = '1' and (TX_MISO_ARY(1).TREADY = '0' or TX_MISO_ARY(2).TREADY = '0' or TX_MISO_ARY(3).TREADY = '0' or TX_MISO_ARY(4).TREADY = '0') then
                error_o <= '1';
             end if;
             
