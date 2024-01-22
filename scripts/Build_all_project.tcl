53551bf38fb1665a2f6b89902f2c45980963b5a7
elarouche-telpos <eric.larouche@telops.com>
2024-01-22 14:48:07 +0000

    build hw (tous les FPAs excepté calcium et isc0804A_2k)
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@29804 3e98269d-ff51-e449-abca-491a6567d830

--- scripts/Build_all_project.tcl
+++ scripts/Build_all_project.tcl
@@ -40,7 +40,7 @@ lappend detector_list {isc0804A325}
 lappend detector_list {isc0804A_500Hz325}
 
 #lappend detector_list {isc0804A_2k160}
-lappend detector_list {isc0804A_2k325}
+#lappend detector_list {isc0804A_2k325}
 
 lappend detector_list {marsD160}
 #lappend detector_list {marsD325}
