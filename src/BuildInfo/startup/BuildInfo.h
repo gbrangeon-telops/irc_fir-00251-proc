95f5f5227127a5859ccd01366a204e70182663c5
elarouche-telpos <eric.larouche@telops.com>
2024-01-26 17:30:36 +0000

    "Pre-release 6.0.x.4"
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@29873 3e98269d-ff51-e449-abca-491a6567d830

--- src/BuildInfo/startup/BuildInfo.h
+++ src/BuildInfo/startup/BuildInfo.h
@@ -15,7 +15,7 @@
 #ifdef ARCH_FPGA_160
  
 #define SVN_HARDWARE_REV 29804
-#define SVN_SOFTWARE_REV      29822
+#define SVN_SOFTWARE_REV      -29823
  #define SVN_BOOTLOADER_REV   29272
  #define SVN_COMMON_REV      29734
  
@@ -28,7 +28,7 @@
 #elif defined(ARCH_FPGA_325)
  
 #define SVN_HARDWARE_REV 29804
-#define SVN_SOFTWARE_REV      29822
+#define SVN_SOFTWARE_REV      29823
  #define SVN_BOOTLOADER_REV   29272
  #define SVN_COMMON_REV      29734
  
