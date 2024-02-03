fd79c78d59d12224662c3aa4683a1c227d4f6103
elarouche-telpos <eric.larouche@telops.com>
2024-02-03 03:35:37 +0000

    "Pre-release 6.1.x.0"
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@29901 3e98269d-ff51-e449-abca-491a6567d830

--- src/BuildInfo/blackbird1920D/BuildInfo.h
+++ src/BuildInfo/blackbird1920D/BuildInfo.h
@@ -28,7 +28,7 @@
 #elif defined(ARCH_FPGA_325)
  
 #define SVN_HARDWARE_REV 29804
-#define SVN_SOFTWARE_REV      29873
+#define SVN_SOFTWARE_REV      29874
  #define SVN_BOOTLOADER_REV   29023
  #define SVN_COMMON_REV      29734
  
