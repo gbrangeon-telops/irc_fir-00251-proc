35f1f046bb60ee33764fcae6caccb1876e1b0bae
alacasse-telpos <alexis.lacasse@telops.com>
2024-03-13 13:26:36 +0000

    Replace encrypted sim_netlist by stub
    git-svn-id: http://einstein/svn/firmware/FIR-00251-Proc/trunk@30087 3e98269d-ff51-e449-abca-491a6567d830

---
+++ IP/325/ip_axis_fi32tofp32/ip_axis_fi32tofp32_stub.vhdl
@@ -0,0 +1,41 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+-- Date        : Wed Mar 13 09:07:29 2024
+-- Host        : TELOPS316 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               D:/Telops/FIR-00251-Proc/IP/325/ip_axis_fi32tofp32/ip_axis_fi32tofp32_stub.vhdl
+-- Design      : ip_axis_fi32tofp32
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7k325tfbg676-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity ip_axis_fi32tofp32 is
+  Port ( 
+    aclk : in STD_LOGIC;
+    aresetn : in STD_LOGIC;
+    s_axis_a_tvalid : in STD_LOGIC;
+    s_axis_a_tready : out STD_LOGIC;
+    s_axis_a_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    s_axis_a_tuser : in STD_LOGIC_VECTOR ( 7 downto 0 );
+    s_axis_a_tlast : in STD_LOGIC;
+    m_axis_result_tvalid : out STD_LOGIC;
+    m_axis_result_tready : in STD_LOGIC;
+    m_axis_result_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    m_axis_result_tuser : out STD_LOGIC_VECTOR ( 7 downto 0 );
+    m_axis_result_tlast : out STD_LOGIC
+  );
+
+end ip_axis_fi32tofp32;
+
+architecture stub of ip_axis_fi32tofp32 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "aclk,aresetn,s_axis_a_tvalid,s_axis_a_tready,s_axis_a_tdata[31:0],s_axis_a_tuser[7:0],s_axis_a_tlast,m_axis_result_tvalid,m_axis_result_tready,m_axis_result_tdata[31:0],m_axis_result_tuser[7:0],m_axis_result_tlast";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "floating_point_v7_1_7,Vivado 2018.3";
+begin
+end;
